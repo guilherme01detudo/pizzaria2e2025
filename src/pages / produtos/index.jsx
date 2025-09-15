d914da0fd7142289b3c25c121435911c6085378a
guilherme01detudo <martinsgui13031991@gmail.com>
2025-09-15 10:55:52 +0000

    listagem de produtos

---
+++ src/pages / produtos/index.jsx
@@ -0,0 +1,17 @@
+const Produtos = () => {
+
+
+    return(
+        <>
+        <h3>Listagem de Produtos</h3>
+        <ul>
+        <li>Pizza de muçarela</li>
+        <li>Pizza de calabresa</li>
+        <li>Pizza Baiana</li>
+        <li>Pizza Portuguesa</li>
+      </ul>
+        </>
+    )
+}
+
+export default Produtos
